c625891a7c83f7245e52bd08a9acd4362e584b7b
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-07-09 22:26:36 -0400

    Final

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,24 +1,47 @@
-class DonutMaker {
-constructor(numDonuts, numAutoClickers) {
-    this.numDonuts=numDonuts;
-    this.numAutoClickers=numAutoClickers;
-    }
+var score = 0;
+var clickAmount = 1;
+var autoclickercost = 100;
+var autoclickers = 0;
+var multipliercost = 10;
+var multipliers = 0;
 
-    addDonut() {
-        this.numDonuts++;
-    }
-    
-    addAutoClicker() {
-        if(this.numDonuts >= 100) {
-            this.numAutoClickers++;
-            this.numDonuts -=100;
-        }
-    }
-    activateAutoClickers() {
-        if (this.numAutoClickers > 0) {
-            this.numDonuts += this.numAutoClickers * 1;
-        }
-    }
+function buyAutoClicker() {
+  if (score >= autoclickercost) {
+    score = score - autoclickercost;
+    autoclickers = autoclickers + 1;
+    autoclickercost = Math.round(autoclickercost * 1.15);
+
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("autoclickercost").innerHTML = autoclickercost;
+    document.getElementById("autoclickers").innerHTML = autoclickers;
+    updateScorePerSecond();
+  }
 }
+function buyMultiplier() {
+    if (score >= multipliercost) {
+      score = score - multipliercost;
+      multipliers = multipliers + 1;
+      multipliercost = Math.round(multipliercost * 1.15);
+  
+      document.getElementById("score").innerHTML = score;
+      document.getElementById("multipliercost").innerHTML = multipliercost;
+      document.getElementById("multipliers").innerHTML = multipliers;
+      updateScorePerSecond();
+    }
+  }
 
-export default DonutMaker;
+function addToScore(amount) {
+  score = score + amount;
+  document.getElementById("score").innerHTML = score;
+}
+function updateScorePerSecond () {
+    scorePerSecond = autoclickers + multipliers * 5;
+    document.getElementById("scorepersecond").innerHTML = scorePerSecond;
+}
+setInterval(function() {
+    score = score + autoclickers;
+    score = score + multipliers * 1.5;
+    document.getElementById("score").innerHTML = score;
+
+    document.title = score + "donuts - Donut Clicker";
+}, 1200);
--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-document.querySelector('section').innerText = 'ITS ALIVE!!!!';
+var score = 0;
+var clickAmount = 1;
+var autoclickercost = 100;
+var autoclickers = 0;
+var multipliercost = 10;
+var multipliers = 0;
+
+function buyAutoClicker() {
+  if (score >= autoclickercost) {
+    score = score - autoclickercost;
+    autoclickers = autoclickers + 1;
+    autoclickercost = Math.round(autoclickercost * 1.15);
+
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("autoclickercost").innerHTML = autoclickercost;
+    document.getElementById("autoclickers").innerHTML = autoclickers;
+    updateScorePerSecond();
+  }
+}
+function buyMultiplier() {
+    if (score >= multipliercost) {
+      score = score - multipliercost;
+      multipliers = multipliers + 1;
+      multipliercost = Math.round(multipliercost * 1.15);
+  
+      document.getElementById("score").innerHTML = score;
+      document.getElementById("multipliercost").innerHTML = multipliercost;
+      document.getElementById("multipliers").innerHTML = multipliers;
+      updateScorePerSecond();
+    }
+  }
+
+function addToScore(amount) {
+  score = score + amount;
+  document.getElementById("score").innerHTML = score;
+}
+function updateScorePerSecond () {
+    scorePerSecond = autoclickers + multipliers * 5;
+    document.getElementById("scorepersecond").innerHTML = scorePerSecond;
+}
+setInterval(function() {
+    score = score + autoclickers;
+    score = score + multipliers * 1.5;
+    document.getElementById("score").innerHTML = score;
+
+    document.title = score + "donuts - Donut Clicker";
+}, 1200);
